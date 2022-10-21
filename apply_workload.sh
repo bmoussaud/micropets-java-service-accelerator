4feb29cbdfdea8c7e678fe895b0b78196521c094
Benoit Moussaud <benoit@moussaud.org>
2022-10-21 12:19:31 +0200

    tap.

--- apply_workload.sh
+++ apply_workload.sh
@@ -1,3 +1,3 @@
-kubectl apply -f config/app-operator
+
 tanzu apps workload apply -f config/workload.yaml --live-update --local-path . --source-image imageRegistry/micropet-tap-lowercasePetKind-sources --namespace dev-tap --yes 
 
