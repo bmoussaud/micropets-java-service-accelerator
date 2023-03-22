f2d6801348e58e28e706f1d48dce831da10b4464
Benoit Moussaud <benoit@moussaud.org>
2023-03-22 10:02:33 +0100

    fix devns

--- apply_workload.sh
+++ apply_workload.sh
@@ -1,3 +1,3 @@
 
-tanzu apps workload apply -f config/workload.yaml --live-update --local-path . --source-image imageRegistry/micropet-tap-lowercasePetKind-sources --namespace dev-tap --yes 
+tanzu apps workload apply -f config/workload.yaml --live-update --local-path . --source-image imageRegistry/micropet-tap-lowercasePetKind-sources --namespace devNamespace --yes 
 
