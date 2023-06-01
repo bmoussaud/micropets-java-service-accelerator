5f2f5a1500c0019cdcae48896f1ea4b1c4150bbc
Benoit Moussaud <benoit@moussaud.org>
2023-06-01 17:06:03 +0200

    observability

--- src/main/java/com/renamethis/PetKindGenerator.java
+++ src/main/java/com/renamethis/PetKindGenerator.java
@@ -2,30 +2,33 @@ package com.renamethis;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+import io.micrometer.observation.annotation.Observed;
+
+@Observed(name = "PetKindGenerator")
+@Service
 public class PetKindGenerator {
 
-    static Logger logger = LoggerFactory.getLogger(PetKindGenerator.class);
+        static Logger logger = LoggerFactory.getLogger(PetKindGenerator.class);
 
-    private PetKindSummary data = new PetKindSummary();
+        private PetKindSummary data = new PetKindSummary();
 
-    PetKindSummary generate() {
+        public PetKindSummary generate() {
 
-        logger.debug("generate data");
-        data.clear();
+                logger.debug("generate data");
+                data.clear();
 
-        data.addPetKind("Tweety", "Yellow Canary", 2,
-                "https://upload.wikimedia.org/wikipedia/en/0/02/Tweety.svg");
-        data.addPetKind("Hector", "African Grey Parrot", 5,
-                "https://petkeen.com/wp-content/uploads/2020/11/African-Grey-Parrot.webp");
-        data.addPetKind("Micheline", "Budgerigar", 3,
-                "https://petkeen.com/wp-content/uploads/2020/11/Budgerigar.webp");
-        data.addPetKind("Piplette", "Cockatoo", 1,
-                "https://petkeen.com/wp-content/uploads/2020/11/Cockatoo.webp");
+                data.addPetKind("Tweety", "Yellow Canary", 2,
+                                "https://upload.wikimedia.org/wikipedia/en/0/02/Tweety.svg");
+                data.addPetKind("Hector", "African Grey Parrot", 5,
+                                "https://petkeen.com/wp-content/uploads/2020/11/African-Grey-Parrot.webp");
+                data.addPetKind("Micheline", "Budgerigar", 3,
+                                "https://petkeen.com/wp-content/uploads/2020/11/Budgerigar.webp");
+                data.addPetKind("Piplette", "Cockatoo", 1,
+                                "https://petkeen.com/wp-content/uploads/2020/11/Cockatoo.webp");
 
-        return data;
+                return data;
 
-    }
+        }
 }
--- src/main/java/com/renamethis/PetKindRepository.java
+++ src/main/java/com/renamethis/PetKindRepository.java
@@ -1,8 +1,7 @@
 package com.renamethis;
 
 import org.springframework.data.repository.CrudRepository;
-public interface PetKindRepository extends CrudRepository<PetKindBean, Long> {
-   
 
+public interface PetKindRepository extends CrudRepository<PetKindBean, Long> {
 
 }
