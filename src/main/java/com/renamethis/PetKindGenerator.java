546e6d7611af4d8f2b7879b2ef591994f1f4c165
Benoit Moussaud <benoit@moussaud.org>
2023-09-12 14:42:10 +0200

    remove value import

--- src/main/java/com/renamethis/PetKindGenerator.java
+++ src/main/java/com/renamethis/PetKindGenerator.java
@@ -3,7 +3,6 @@ package com.renamethis;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import io.micrometer.observation.annotation.Observed;
