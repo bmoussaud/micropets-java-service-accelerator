53b1efb17019215e30cd8814d509c6e92746c1c8
Benoit Moussaud <benoit@moussaud.org>
2023-09-21 11:23:20 +0200

    add InMemoryHttpExchangeRepository

--- src/main/java/com/renamethis/Application.java
+++ src/main/java/com/renamethis/Application.java
@@ -1,7 +1,10 @@
 package com.renamethis;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -11,5 +14,11 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Bean
+    public HttpExchangeRepository htttpTraceRepository() {
+        return new InMemoryHttpExchangeRepository();
+    };
+
 
 }
